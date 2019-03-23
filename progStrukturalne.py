a4cdcc54f9fdacf53b3cb0c2fdea4082a3583728
marcinfoltyn <marcinfoltyn@o2.pl>
2019-03-23 14:00:24 +0100

    pierwszy program

---
+++ progStrukturalne.py
@@ -0,0 +1,39 @@
+print("CDV")
+print("hello world")
+#potega
+potega = 2 ** 10
+
+print(potega)
+tekst = "cdv"
+
+tekst = "cdv"
+print (tekst* 2)
+#pobieranie danych z klawiatury
+imie = input()
+print("Twoje imie podane z klawiatury " + imie)
+nazwisko = 'nowak'
+print("twoje dane: " + imie " " + nazwisko)
+wiek = input()
+print("wiek: " +wiek)
+print(type(imie))
+print(type(nazwisko))
+wiekAla = 30
+print(type(wiekAla)) #int
+wiekAla = str (wiekAla)
+print(type(wiekAla))
+print("wiek ali" +wiekAla)
+pierwszyZnak = nazwisko[0]
+print(pierwszyZnak)
+ostatniZnak = nazwisko (len(nazwisko -1))
+print(ostatniZnak)
+#konwersja
+x = "5"
+print(type(x))
+x = int(x)
+print(type (x))
+y = 4
+print(type(y))
+y = 4 / 2
+# y = float(y)
+print(type(y))
+print(y)
